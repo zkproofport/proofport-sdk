40528ac5d9dfcf9b987344a307eecde5dcb80389
hsy822 <hyunsy822@gmail.com>
2025-11-09 16:02:56 +0900

    remove /

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const PROOF_PORTAL_URL = "https://zkproofport.com/portal/";
+export const PROOF_PORTAL_URL = "https://zkproofport.com/portal";
 export const ALLOWED_ORIGIN = "https://zkproofport.com";
